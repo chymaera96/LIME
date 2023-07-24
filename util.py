0f665c461c6165a29ae1656d31f1850e6c1049c7
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-07-24 19:46:47 +0100

    Lyrics alignment + preprocessing pipeline

--- util.py
+++ util.py
@@ -1,11 +1,15 @@
 import librosa
 import soundfile as sf
+import yaml
 
 def load_audio(path, sr=16000):
     try:
-        audio, sr = librosa.load(path, sr=sr, mono=True)
+        audio, sr = librosa.load(path, sr=sr, mono=False)
     except Exception as e:
         audio, sr = sf.read(path)    
     return audio, sr
 
-        
+def load_config(config_path):
+    with open(config_path, 'r') as stream:
+        config = yaml.safe_load(stream)
+    return config
