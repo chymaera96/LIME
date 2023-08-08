smooth_ssm computation + lyr_vec size fix

preprocess.py
@@ -24,7 +24,7 @@ def extract_stems(audio, separator=None):
     return stems
 
 
-def extract_lyrics_vectors(ala_file=None, ala=None, max_dur=5.0):
+def extract_lyrics_vectors(ala_file=None, ala=None, size=None, max_dur=5.0):
     if ala is None:
         ala = pd.read_csv(ala_file, header=['start','end','text'])
     
@@ -36,7 +36,10 @@ def extract_lyrics_vectors(ala_file=None, ala=None, max_dur=5.0):
     e_t = np.round(list(df['end']*2))
     texts = df['text']
     vocab = list(set(texts))
-    lvecs = np.zeros((len(vocab),int(np.round(list(ala['end']*2))[-1])))
+    if size is None:
+        lvecs = np.zeros((len(vocab),int(np.round(list(ala['end']*2))[-1])))
+    else:
+        lvecs = np.zeros((len(vocab),int(np.ceil(size*2))))
     inv_w_ix = {k:v for v, k in enumerate(vocab)}
 
     for i in range(len(df)):

train.py
@@ -39,9 +39,10 @@ def train(cfg, train_loader, model, optimizer, augment=None):
         with torch.no_grad():
             S = augment(S) if augment is not None else S
         output = model(S)
-
-        loss1 = F.mse_loss(output, I1)
-        loss2 = F.mse_loss(output, I2)
+        emb_ssm = compute_smooth_ssm(output)
+        assert emb_ssm.shape == I1.shape == I2.shape
+        loss1 = F.mse_loss(emb_ssm, I1)
+        loss2 = F.mse_loss(emb_ssm, I2)
         loss = loss1 + cfg['gamma'] * loss2
 
         loss.backward()