de506d47888d1cadf8b9d117e2894d6caa15e5f8
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-08-16 23:14:27 +0100

    config1_2

--- model/loss.py
+++ model/loss.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 
 def weighted_mse_loss(emb_SSM, label_SSM, audio_len):
     batch_size = emb_SSM.shape[0]
-    weights = torch.reciprocal(audio_len.float()) ** 2
+    weights = torch.reciprocal(audio_len.float())
     sq_error = (emb_SSM - label_SSM) **2
     sq_error = sq_error.view(batch_size, -1)
     loss = torch.sum(sq_error * weights.view(-1, 1), axis=1)
@@ -11,7 +11,7 @@ def weighted_mse_loss(emb_SSM, label_SSM, audio_len):
 
 def assymetric_loss(emb_SSM, label_SSM, audio_len):
     batch_size = emb_SSM.shape[0]
-    weights = torch.reciprocal(torch.Tensor(audio_len).float()) ** 2
+    weights = torch.reciprocal(torch.Tensor(audio_len).float())
     error = F.relu(label_SSM - emb_SSM)
     error = error.view(batch_size, -1)
     loss = torch.sum(error * weights.view(-1, 1), axis=1)
