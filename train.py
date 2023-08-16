82228613064fc2e8f459482606d4199f354f4bce
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-08-16 19:35:13 +0100

    bug fix

--- train.py
+++ train.py
@@ -50,7 +50,8 @@ def train(cfg, train_loader, model, optimizer, augment=None):
         if not emb_ssm.shape == I1.shape == I2.shape:
             print(f"Shapes of emb_ssm, I1, I2: {emb_ssm.shape}, {I1.shape}, {I2.shape}")
             print(f" input shape: {S.shape}")
-        loss1 = assymetric_loss(emb_ssm, I1, L)
+        # loss1 = assymetric_loss(emb_ssm, I1, L)
+        loss1 = weighted_mse_loss(emb_ssm, I1, L)
         loss2 = weighted_mse_loss(emb_ssm, I2, L)
         loss = cfg['gamma'] * loss1 + (1 - cfg['gamma']) * loss2
 
