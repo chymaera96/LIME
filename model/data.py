c5b855907f18ec52346d64335f97f9761be8a6fd
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-08-16 12:38:37 +0100

    Issue: network rounds down when fractional < 1/16

--- model/data.py
+++ model/data.py
@@ -71,6 +71,10 @@ class LIMEDataset(Dataset):
         if self.transform is not None:
             cqt = self.transform(cqt)
 
+        # Pad CQT for edge cases
+        if cqt.shape[-1] / 16 % 1 <= 0.0625:
+            cqt = np.pad(cqt, ((0,0), (0,0), (0,0), (0,1)), mode='constant', constant_values=0)
+
         # Checking number of frames in crema_pcp and lyr_enc
         if crema_pcp.shape[1] != lyr_enc.shape[1]:
             print(f"crema_pcp.shape[1] - lyr_enc.shape[1] = {crema_pcp.shape[1] - lyr_enc.shape[1]}")
