013795a492d0fae7466a45da6f4b7096973d45ed
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-09-05 19:15:04 +0100

    minor fix

--- test.py
+++ test.py
@@ -96,8 +96,8 @@ def main():
                 plt.close()
             audThumb = FastThumbnail(cfg=cfg)
             cov, rep = audThumb(S)
-            print(f"Ssm shape: {S.shape}")
-            print(rep.shape)
+            # print(f"Ssm shape: {S.shape}")
+            # print(rep.shape)
             scape = compute_scape_plot(ssm, fitness=rep)
             if ix == 0 and args.scape_plot:
                 plt.imshow(scape, cmap='hot', origin='lower')
