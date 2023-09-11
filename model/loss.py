5d6d7ff72bfb87fa34da4ed8db90fff747b76ec3
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-09-11 16:00:23 +0100

    config3_0

--- model/loss.py
+++ model/loss.py
@@ -9,7 +9,7 @@ def weighted_mse_loss(emb_SSM, label_SSM, audio_len):
     loss = torch.sum(sq_error * weights.view(-1, 1), axis=1)
     return torch.mean(loss)
 
-def assymetric_loss(emb_SSM, label_SSM, audio_len):
+def asymmetric_loss(emb_SSM, label_SSM, audio_len):
     batch_size = emb_SSM.shape[0]
     weights = torch.reciprocal(torch.Tensor(audio_len).float())
     error = F.relu(label_SSM - emb_SSM)
