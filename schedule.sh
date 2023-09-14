89acc51a6868f3fe941e75d1e6d04291c21e33b6
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-09-14 11:16:12 +0100

    schedule update

--- schedule.sh
+++ schedule.sh
@@ -1,2 +1,2 @@
-python train.py --config=config/dali.yaml --ckp=lime_config4_5
-python test.py --test_dir=/import/c4dm-datasets/SALAMI/audio --preprocess=False --match=config4_5
+python train.py --config=config/dali.yaml --ckp=lime_config4_7
+python test.py --test_dir=/import/c4dm-datasets/SALAMI/audio --preprocess=False --match=config4_7
